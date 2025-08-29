9288f73afb556c2a1ab863b1d3620cb5d9dbdb8f
tanmay-cdac <tanmay.sawant.cmaug25@gmail.com>
2025-08-29 19:15:54 +0530

    Assignment 1 submitted

--- Assignment 1/GradeSwitch.java
+++ Assignment 1/GradeSwitch.java
@@ -1,21 +1,20 @@
 import java.util.*;
 
-class GradeSwitch {
+class GradeSwitch{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter marks (0-100): ");
+        System.out.print("Enter marks (0–100): ");
         int marks = sc.nextInt();
 
-        String grade;
-        switch (marks / 10) {
-            case 10, 9, 8, 7 -> grade = "A";
-            case 6 -> grade = "B";           
-            case 5 -> grade = "C";           
-            case 4 -> grade = "D";          
-            case 3, 2 -> grade = "E";        
-            default -> grade = "F";          
-        }
+        String grade = switch (marks / 10) {
+            case 10, 9, 8 -> "A"; 
+            case 7 -> "B";        
+            case 6 -> (marks >= 55) ? "C" : "D"; 
+            case 5, 4 -> (marks >= 45) ? "D" : "E"; 
+            case 3, 2 -> "E";   
+            case 1, 0 -> "F";     
+            default -> "Invalid Marks!";
+        };
 
         System.out.println("Grade: " + grade);
     }
